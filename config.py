0791f8e01860ea6928537bd364a192b6df62bb5d
Aryan Falahatpisheh <aryan.falahatpisheh@berkeley.edu>
2015-07-15 13:33:04 -0700

    Created config.py to house stickman texts. Will need to be fleshed out later

---
+++ config.py
@@ -0,0 +1,62 @@
+
+usuage = """
+help - Prints commands
+auto <knower/guesser> - will make one of these automatic
+start - starts the game
+guess <letter> - guesses this letter
+hint - prints the letter that the AI would pick
+word <new_word> - Restarts the game if in progress and sets this as the word
+clear - Clears the screen
+"""
+
+stickman = [
+
+"""
+
+
+
+
+""",
+
+"""
+ O 
+
+
+
+""",
+
+"""
+ O
+ |
+
+
+""",
+
+"""
+_O
+ |
+
+
+""",
+
+"""
+_O_
+ |
+
+
+""",
+
+"""
+_O_
+ |
+/ 
+
+""",
+
+"""
+_O_
+ |
+/ \\
+
+"""
+]
